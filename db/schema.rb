0c5a1922b713a6497acc7f0acb1239d28018b75a
Tegan <teganoar0@gmail.com>
2019-05-05 16:21:39 +1000

    added form and created edit.html.erb  show.html.erb and new.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_035238) do
+ActiveRecord::Schema.define(version: 2019_05_05_060129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2019_05_05_035238) do
     t.string "name"
     t.text "description"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sellers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
