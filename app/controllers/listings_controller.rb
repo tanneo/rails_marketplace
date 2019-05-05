4f9d7e1ba839c8ee095b39e53d665e7e412d1d8a
Tegan <teganoar0@gmail.com>
2019-05-06 09:11:13 +1000

    Updated form to create new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,27 @@
 class ListingsController < ApplicationController
+    before_action :set_listing, only: [:image, :show, :edit, :update, :destroy]
+
     def index
-        @listings = Listing.all
+      @listings = Listing.all
+    end
+  
+    def show
     end
 
-    def new
-
+    def edit
     end
-end
+  
+    def create
+    #   @listing = Listing.new(listing_params)
+    end
+  
+#     private
+#       def set_listing
+#         @listing = Listing.find(params[:id])
+#       end
+  
+ 
+#       def listing_params
+#         params.require(:listing).permit(:name, :description, :price,)
+#       end
+  end
