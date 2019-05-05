adf3c64ee0867c44c9951508909edcca8057d52a
Tegan <teganoar0@gmail.com>
2019-05-06 09:36:42 +1000

    added destroy method to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -15,11 +15,19 @@ class ListingsController < ApplicationController
       @listing = Listing.create(listing_params)
       redirect_to listings_path
     end
+
+    def destroy
+        @listing.destroy
+        respond_to do |format|
+          format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
   
-#     private
-#       def set_listing
-#         @listing = Listing.find(params[:id])
-#       end
+    private
+      def set_listing
+        @listing = Listing.find(params[:id])
+      end
   
  
       def listing_params
