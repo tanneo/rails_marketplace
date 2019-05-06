cccfab7c1aa91b31f2af7d80621ed3d5bb852caf
Tegan <teganoar0@gmail.com>
2019-05-06 11:35:40 +1000

    self added configuration to development.rb from video tutorial for active storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+    #SELF ADDED
+    config.active_storage.service = :local 
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # for devise
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
