70a7ca7ca7e92e526a6421a566e48112274a4aa7
Tegan <teganoar0@gmail.com>
2019-05-06 11:24:07 +1000

    updated new seeds file to now incluce images, updated model for images and updated lin_form.html to include image. Uploaded images to images folder under assets

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,23 @@
 class Listing < ApplicationRecord
     has_one_attached :image
+
+    def thumbnail 
+        return self.image.variant(resize: "100x100")
+    end
+
+    def medium
+        return self.image.variant(resize: "200x200")
+    end
+
+    # validates data fields 
+    validates :name, :description, :price, :image, presence: true
+    validates :price, numericality: {greater_than: 0 }
+end
+
+def correct_image_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+        errors.add(:image, 'must be a JPEG or PNG.')
+    elsif image.attached? == false
+        errors.add(:image, 'must have an image attached')
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-  
-listings = [
-    {
-        name: 'Dog 2',
-        description: 'This is the first post!',
-        price: 300
-    },
-    {
-        name: 'Dog 3',
-        description: 'To be or not to be',
-        price: 200
-    },
-    {
-        name: 'Dog 4',
-        description: 'There\'s a bat in my belfry',
-        price: 300
-    }
-]
+listing1 = Listing.create(
+    name: "Molly",
+    description: "Molly is a great dog. ",
+    price: 1000,
+    )
+listing1.image.attach(io: File.open("app/assets/images/dog1.png"), filename:'dog1.png', content_type: 'image/png')
 
-Listing.create!(listings) { |item| puts "#{item.name} created." }
+listing2 = Listing.create(
+    name: "Bonnie and Clyde",
+    description: "Bonnie and Clyde are both great dogs. ",
+    price: 5000,
+)
+listing2.image.attach(io: File.open("app/assets/images/dog2.png"), filename:'dog2.png', content_type: 'image/png')
+
+listing3 = Listing.create(
+    name: "Myffy",
+    description: "Myffy is a great dog. ",
+    price: 2500,
+)
+listing3.image.attach(io: File.open("app/assets/images/dog3.png"), filename:'dog3.png',content_type: 'image/png')
+
+listing4 = Listing.create(
+    name: "Nelly",
+    description: "Nelly is a great dog. ",
+    price: 9000,
+)
+listing4.image.attach(io: File.open("app/assets/images/dog4.png"), filename:'dog4.png', content_type: 'image/png')
