8e20ef9b587853113e66863f62e3723c9b1f0701
Tegan <teganoar0@gmail.com>
2019-05-10 13:50:07 +1000

    setting up keys for cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   #SELF ADDED TO SETUP DEVISE
    # Required for Devise. Remember to change localhost:3000 to actual application host
    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  end #for devise
+  # end #for devise
 
   #SELF ADDED TO SETUP CLOUDINARY
   config.active_storage.service =:cloudinary
